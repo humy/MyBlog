72b0b9c53337f4668d584895bb83637ee7dbe192
humy <minyan_hu@126.com>
2020-03-14 16:55:13 +0800

    delete some sentences

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Prism",
+    "name": "Kepler",
     "type": "local",
-    "path": "../Templates/Maverick-Theme-Prism/"
+    "path": "../Templates/Maverick-Theme-Kepler/"
 }
 
 index_page_size = 5
